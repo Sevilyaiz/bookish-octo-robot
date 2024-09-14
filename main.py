e83bac12c520c77770d4f8fc7ee0d7de79b99dd8
Sevilyaiz <Sevilyaiz>
2024-09-14 20:14:48 +0500

    first

---
+++ main.py
@@ -0,0 +1,75 @@
+# type = 'Bugatti'
+# color = 'white'
+# max_speed  = 300
+
+# class laptops :
+#    def __init__(self,model,info,year):
+#        self.model = model
+#        self.info = info
+#        self.year = year
+#
+#    def buy(self):
+#        print('Товар в корзине')
+#
+# hp =  laptops(model='envy',info='500 ram',year=2022 )
+# hp.buy()
+
+
+# class BankAccaunt
+#
+#     def __init__(self,name,balance = 0):
+#         self.name = name
+#         self.balance = balance
+#     def  deposit(self,cash_amount):
+#         self. balance +=cash_amount
+#
+#     def cash(self,cash_amount):
+#         self.balance -= cash_amount
+#
+#     def my_balance(self):
+#          return self.balance
+
+class User:
+    def __init__(self, name,email,age,phone_number):
+      self.name = name
+      self.email = email
+      self.age  = age
+      self.phone = phone_number
+
+    def change_user_name(self,name):
+      self.name = new_name
+
+
+    def  change_number(self,number):
+      self.number = new_number
+
+
+
+    def change_email(self,email):
+      self.email = new_email
+
+
+andrey =User('Alex','al1o',20,999757578)
+print(andrey.name)
+name_change =input('Введите новое имя:')
+
+
+
+
+
+
+
+
+
+
+# class Coment :
+#     def __init__(self,username,text,likes= 0):
+#         self.username = username
+#         self.text = text
+#         self.likes =likes
+#
+# instagramm =input('Введите ваш ник '),input('Введите текст '),input(0)
+# print(instagramm)
+
+
+
